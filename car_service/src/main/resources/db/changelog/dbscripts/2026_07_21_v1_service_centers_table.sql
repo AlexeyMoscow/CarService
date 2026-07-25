69e4539fe1d48789b35443acda846d1a0a7618a4
Alexey <alex.nowik@mail.ru>
2026-07-25 11:29:11 +0400

    add initial tables for car service application including car_services, car_owners, and service_centers

--- car_service/src/main/resources/db/changelog/dbscripts/2026_07_21_v1_service_centers_table.sql
+++ car_service/src/main/resources/db/changelog/dbscripts/2026_07_21_v1_service_centers_table.sql
@@ -1,1 +1,9 @@
-CREATE TABLE cars1
+CREATE TABLE service_centers
+(
+    id         UUID                     DEFAULT RANDOM_UUID() PRIMARY KEY,
+    name       VARCHAR(255)             NOT NULL,
+    address    VARCHAR(255)             NOT NULL,
+    phone      VARCHAR(255),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
