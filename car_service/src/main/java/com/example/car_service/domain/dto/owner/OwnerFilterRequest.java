dc7e81c335b6096f8ec4c87f58f83336423f9dc5
Alexey <alex.nowik@mail.ru>
2026-07-30 11:41:19 +0400

    Review comments fix + OwnerFilterRequest (2 fields added + validation)

--- car_service/src/main/java/com/example/car_service/domain/dto/owner/OwnerFilterRequest.java
+++ car_service/src/main/java/com/example/car_service/domain/dto/owner/OwnerFilterRequest.java
@@ -1,12 +1,46 @@
 package com.example.car_service.domain.dto.owner;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
 public record OwnerFilterRequest(
 
+        @Size(
+                min = 2, max = 100,
+                message = "Full name must contain from 2 to 100 characters")
+        @Pattern(
+                regexp = ".*\\S.*",
+                message = "Full name must not be blank"
+        )
         String fullName,
 
+
+        @Pattern(
+                regexp = "^7\\d{10}$",
+                message = "Phone must start with 7 and contain 11 digits"
+        )
         String phone,
 
-        String email
+
+        @Email(
+                message = "Email has invalid format")
+        @Pattern(
+                regexp = ".*\\S.*",
+                message = "Email must not be blank"
+        )
+        String email,
+
+        @PastOrPresent(
+                message = "Updated from must not be in the future")
+        LocalDate updatedFrom,
+
+        @PastOrPresent(
+                message = "Updated from must not be in the future")
+        LocalDate updatedTo
 
 ) {
 }
